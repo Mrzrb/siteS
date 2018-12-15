bde0cf3d2d2fd86690a139227b5fd09a8e8b079c
张若彬 <zhangruobin@outlook.com>
2018-12-15 17:16:36 +0800

    fix

--- database/migrations/2018_10_27_161142_create_prod_table.php
+++ database/migrations/2018_10_27_161142_create_prod_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductTable extends Migration
+class CreateProdTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('prods', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255)->default('');
             $table->integer('price')->default(0);
@@ -31,6 +31,6 @@ class CreateProductTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('prod');
     }
 }
